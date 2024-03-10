bb763e298546e3fb9494c69b5835aead8a9c8ce9
Bimsara Walallawita <bwalallawita@gmail.com>
2024-03-10 13:02:49 +0530

    React Crud

--- src/App.js
+++ src/App.js
@@ -1,25 +1,186 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import axios from 'axios';
 
-function App() {
+function App() { //Root Component
+
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState("");
+  const [users, setUsers] = useState(null);
+
+  function getUsers() {
+    axios.get("http://localhost:8080/users")
+      .then(function (response) {
+        setUsers(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  /* function increase() {
+    setCount(count + 1);
+    console.log(count);
+  } */
+
+  const increase = () => {
+    setCount(count + 1);
+  }
+
+  function handleText(event) {
+    setText(event.target.value);
+  }
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+
+  function handleUsername(event) {
+    setUsername(event.target.value);
+  }
+
+  function handlePassword(event) {
+    setPassword(event.target.value);
+  }
+
+  function handleEmail(event) {
+    setEmail(event.target.value);
+  }
+
+  function createUser(event) {
+    event.preventDefault();
+
+    const data = {
+      username: username,
+      password: password,
+      email: email
+    }
+
+    axios.post("http://localhost:8080/users", data)
+      .then(function (response) {
+        console.log(response);
+        getUsers();
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  const [edit, setEdit] = useState(null);
+
+  function updateUser(event) {
+    event.preventDefault();
+
+    const data = {
+      username: username,
+      password: password,
+      email: email
+    }
+
+    axios.put("http://localhost:8080/users/" + edit.id, data)
+      .then(function (response) {
+        console.log(response);
+        getUsers();
+        setEdit(null);
+      })
+      .catch(function (error) {
+        console.log(error);
+      }
+      );
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello React!</h1>
+
+      {users && users.map((user) => (
+        <div key={user.id}>
+          {user.username} - {user.email}
+          <button type="button" onClick={
+            () => {
+              setEdit(user);
+              setUsername(user.username);
+              setEmail(user.email);
+              setPassword(user.password);
+            }
+          }>Edit</button>
+          <button type="button" onClick={
+            () => {
+              axios.delete("http://localhost:8080/users/" + user.id)
+              .then(function () {
+                getUsers();
+              })
+              .catch(function (error) {
+                console.log(error);
+              });
+            }
+          }>Delete</button>
+        </div>
+      ))
+      }
+
+      <button onClick={getUsers}>Load Users</button>
+
+      {!edit &&
+        <form onSubmit={createUser}>
+
+          <h1>Create User</h1>
+
+          <div>
+            <label>Username</label>
+            <input type="text" onChange={handleUsername} required />
+          </div>
+
+          <div>
+            <label>Password</label>
+            <input type="password" onChange={handlePassword} required />
+          </div>
+
+          <div>
+            <label>Email</label>
+            <input type="email" onChange={handleEmail} required />
+          </div>
+
+          <button type="submit">Save</button>
+        </form>
+      }
+
+      {edit &&
+        <form onSubmit={updateUser}>
+          <h1>Edit User</h1>
+
+          <div>
+            <label>Username</label>
+            <input type="text" onChange={handleUsername} value={username} required />
+          </div>
+
+          <div>
+            <label>Password</label>
+            <input type="password" onChange={handlePassword} value={password} required />
+          </div>
+
+          <div>
+            <label>Email</label>
+            <input type="email" onChange={handleEmail} value={email} required />
+          </div>
+
+          <button type="submit">Save</button>
+          <button type="button" onClick={
+            () => {
+              setEdit(null);
+            }
+          }>Cancel</button>
+        </form>
+      }
     </div>
   );
+}
+
+function ListItem({ title, price }) {
+  return (
+    <div>{title}</div>
+  )
 }
 
 export default App;
